dfc041316d59757ea1f7dd665e96a90800b54c1e
leo-1997 <36431769+leo-1997@users.noreply.github.com>
2019-07-19 13:57:56 +0800

    set checkAnserr()

--- app/src/main/java/com/example/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -32,28 +32,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mQuestionTextView = findViewById(R.id.question_text_view);
-        int mQuestionId = mQuestionBank[mCurrentQuestionIndex].getTextResId();
-        mQuestionTextView.setText(mQuestionId);
 
         mTrueButton = findViewById(R.id.true_button);
         mFalseButton = findViewById(R.id.false_button);
+        mNextButton = findViewById(R.id.next_button);
 
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast toast = Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.TOP, 0, 0);
-                toast.show();
+                checkAnswer(true);
             }
         });
 
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast toast = Toast.makeText(MainActivity.this, R.string.wrong_toast, Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.TOP, 0, 0);
-                toast.show();
+                checkAnswer(false);
             }
         });
+
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                mCurrentQuestionIndex = (++mCurrentQuestionIndex) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+        updateQuestion();
+    }
+
+    private void updateQuestion() {
+        mQuestionTextView.setText(mQuestionBank[mCurrentQuestionIndex].getTextResId());
+    }
+
+    private void checkAnswer(boolean userAnswer) {
+        boolean answerIsTrue = mQuestionBank[mCurrentQuestionIndex].isAnswerTrue();
+
+        int messageResId;
+
+        messageResId = (userAnswer == answerIsTrue ? R.string.correct_toast : R.string.wrong_toast);
+
+        Toast toast = Toast.makeText(MainActivity.this, messageResId, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.TOP, 0, 0);
+        toast.show();
     }
 }

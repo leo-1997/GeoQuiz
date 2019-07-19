7cc3c7409a73f54a419813fb0bf0faf155211307
leo-1997 <36431769+leo-1997@users.noreply.github.com>
2019-07-19 13:26:26 +0800

    setQuestionBank

--- app/src/main/java/com/example/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -5,27 +5,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    private Question[] mQuestionBank = {new Question(R.string.question_australia, true),
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true),
+};
+
+    private int mCurrentQuestionIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mQuestionTextView = findViewById(R.id.question_text_view);
+        int mQuestionId = mQuestionBank[mCurrentQuestionIndex].getTextResId();
+        mQuestionTextView.setText(mQuestionId);
+
         mTrueButton = findViewById(R.id.true_button);
         mFalseButton = findViewById(R.id.false_button);
 
         mTrueButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT)
-                        .show();
-            }
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT)
+                            .show();
+                }
         });
 
         mFalseButton.setOnClickListener(new View.OnClickListener() {
